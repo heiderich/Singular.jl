0773cd1e78dd1f296af914990809e8efb779c7de
Sebastian Gutsche <gutsche@mathematik.uni-siegen.de>
2019-06-11 11:52:38 +0200

    Added post-functions

--- src/libsingular/Meta.jl
+++ src/libsingular/Meta.jl
@@ -1,5 +1,11 @@
 include("libraryfuncdictionary.jl")
 
+output_maniuplator_funcs = Dict(
+    :dummy => Dict(
+        # :dummy => i -> i[1]
+    )
+)
+
 for (name,funcs) in libraryfunctiondictionary
     name_caps = Symbol(uppercase(string(name)))
     func_calls = Any[]
@@ -8,9 +14,13 @@ for (name,funcs) in libraryfunctiondictionary
         if i[1] == "g"
             func_name = i[2]
             symb = Symbol(func_name)
-            # push!(func_calls, name = :($name))
-            push!(func_calls, :($symb(args...) = libSingular.low_level_caller($(name_string),$func_name,args...)))
-            push!(func_calls, :($symb(ring::PolyRing,args...) = libSingular.low_level_caller_rng($(name_string),$func_name,ring,args...)))
+            if haskey(output_maniuplator_funcs,name) && haskey(output_maniuplator_funcs[name],symb)
+                push!(func_calls, :($symb(args...) = $(output_maniuplator_funcs[name][symb])(libSingular.low_level_caller($(name_string),$func_name,args...)) ))
+                push!(func_calls, :($symb(ring::PolyRing,args...) = $(output_maniuplator_funcs[name][symb])(libSingular.low_level_caller_rng($(name_string),$func_name,ring,args...)) ))
+            else
+                push!(func_calls, :($symb(args...) = libSingular.low_level_caller($(name_string),$func_name,args...)))
+                push!(func_calls, :($symb(ring::PolyRing,args...) = libSingular.low_level_caller_rng($(name_string),$func_name,ring,args...)))
+            end
         end
     end
     eval(:(baremodule $name_caps
