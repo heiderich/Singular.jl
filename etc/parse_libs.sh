2575ce44fc826b1f6b2c9f90f842fd3230215c06
Sebastian Gutsche <gutsche@mathematik.uni-siegen.de>
2019-06-18 20:31:37 +0200

    Hooked library parser into build script

--- etc/parse_libs.sh
+++ etc/parse_libs.sh
@@ -2,4 +2,4 @@
 
 ## create json for all files
 
-/home/sebastian/Software/Singular-git/Singular/libparse -f $1 | awk -F " " '{print "[\"" $1 "\",\"" $3 "\"]\,"}' | tail -n+4
+$1 -f $2 | awk -F " " '{print "[\"" $1 "\",\"" $3 "\"]\,"}' | tail -n+4
