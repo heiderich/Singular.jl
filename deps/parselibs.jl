49883982498fc4f8a753f52dde35a214791038f4
Sebastian Gutsche <gutsche@mathematik.uni-siegen.de>
2019-06-24 17:09:14 +0200

    Parse libs in julia directly, without using awk

--- deps/parselibs.jl
+++ deps/parselibs.jl
@@ -11,7 +11,6 @@ function execute(cmd::Cmd)
       code = process.exitcode)
 end
 
-parsepath = abspath(joinpath(@__DIR__, "..", "etc", "parse_libs.sh"))
 libparsepath = abspath(joinpath(@__DIR__, "..", "local", "bin", "libparse"))
 
 library_dir = ""
@@ -29,12 +28,19 @@ output_filename = abspath(joinpath(@__DIR__, "..", "src", "libraryfuncdictionary
 open(output_filename, "w") do outputfile
     println(outputfile, "libraryfunctiondictionary = Dict(")
     for i in filenames
-    full_path = joinpath(library_dir, i)
-    libs = execute(`$parsepath $libparsepath $full_path`)
-    println(outputfile, """
-        :$(i[1:end - 4]) => [ 
-        $(libs.stdout)],
-        """)
-end
+        full_path = joinpath(library_dir, i)
+        libs = execute(`$libparsepath $full_path`)
+        if libs.stderr != ""
+            error("from libparse: $(libs.stderr)")
+        end
+        libs_splitted = split(libs.stdout,"\n")[4:end-1]
+        libs_splitted = [ split(i," ") for i in libs_splitted ]
+        libs_splitted = [ [ j for j in i if j != ""] for i in libs_splitted ]
+        println(outputfile, ":$(i[1:end - 4]) => [")
+        for j in libs_splitted
+            println(outputfile, """[ "$(j[1])", "$(j[3])" ],""")
+        end
+        println(outputfile, "],\n")
+    end
     println(outputfile, ")\n")
 end
--- etc/parse_libs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-## create json for all files
-
-$1 -f $2 | awk -F " " '{print "[\"" $1 "\",\"" $3 "\"]\,"}' | tail -n+4
